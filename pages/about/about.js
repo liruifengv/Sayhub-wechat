ff3222514533e65213f91f0a3afa9c3a9c29e82a
liruifengv <li15227870916@gmail.com>
2017-11-22 22:37:22 +0800

    first commit && complete about page

---
+++ pages/about/about.js
@@ -0,0 +1,40 @@
+// pages/about/about.js
+Page({
+    data: {
+        members: [
+            {
+                name: 'liruifengv',
+                icon: '/images/members/liruifengv.jpg',
+                desc: '来生再修好皮囊'
+            }
+        ]
+    },
+    onLoad: function (options) {
+        // 页面初始化 options为页面跳转所带来的参数
+        console.log('关于页加载成功')
+    },
+    onReady: function () {
+        // 页面渲染完成
+    },
+    onShow: function () {
+        // 页面显示
+    },
+    onHide: function () {
+        // 页面隐藏
+    },
+    onUnload: function () {
+        // 页面关闭
+    },
+    onShareAppMessage: function () {
+        return {
+            title: '在这里畅所欲言',
+            path: '/pages/about/about',
+            success: function (res) {
+                // 分享成功
+            },
+            fail: function (res) {
+                // 分享失败
+            }
+        }
+    }
+})
